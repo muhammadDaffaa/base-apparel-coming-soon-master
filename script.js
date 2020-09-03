6fc75f3fa25b560ec62343045da69fc20c60fc28
muhammaddaffa06 <66205201+muhammaddaffa06@users.noreply.github.com>
2020-09-03 09:29:47 +0700

    Upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// const input = document.querySelector("#input");
+const button = document.querySelector("#button");
+let string = "e"
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  document.getElementById("#text3").innerHTML = "<h1>INI BARU</h1>";
+  // hasil.style.innerHTML = "<h1>Program belum selesai</h1>"
+})
